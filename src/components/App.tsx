1bc38db2c3dc3f004172c2adcc03f494215e1bca
Isolumi <hubertx98@gmail.com>
2023-07-17 21:13:21 -0400

    complete firebase setup

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default App
