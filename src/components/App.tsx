6938ae1112ee1339720e0a46b97e324ef8a1e8a4
Isolumi <hubertx98@gmail.com>
2023-07-18 23:04:21 -0400

    completed signup page TODO: need to add success notification

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
-import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './Signup';
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
   return (
-    <Signup />
-  )
+    <AuthProvider>
+      <Container className="min-h-screen d-flex align-items-center justify-content-center">
+        <div className="w-full max-w-[400px]">
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
+  );
 }
 
 export default App;
